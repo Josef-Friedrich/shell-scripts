0eb7a67c4a11bcf7e0f219b1d3058607af3476e0
Josef Friedrich <josef@friedrich.rocks>
2016-10-24 08:41:27 +0200

    Add new scripts

---
+++ rm-latex-tmp-files.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+
+# MIT License
+#
+# Copyright (c) 2016 Josef Friedrich <josef@friedrich.rocks>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+EXTENSIONS="aux
+fdb_latexmk
+log
+out
+synctex.gz
+toc"
+
+SEARCH=""
+PROMPT=""
+for EXTENSION in $EXTENSIONS; do
+  SEARCH="$SEARCH -name \"*.$EXTENSION\" -or "
+  PROMPT="$PROMPT \"$EXTENSION\""
+done
+
+echo -n "Delete files in the directory \"$(pwd)\" with the extensions$PROMPT (y|n)? "
+
+read YES
+
+if [ "$YES" != "y" ]; then
+  echo "User abortion."
+  exit 1
+fi
+
+SEARCH=${SEARCH:0:${#SEARCH}-4}
+
+COMMAND="find . \\( $SEARCH \\) -exec rm -ifv {} \\;"
+
+eval $COMMAND
+
+exit 0
