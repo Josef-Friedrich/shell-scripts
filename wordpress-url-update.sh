67ee6d072e3f2e2c809015014a7d9b9d79b9f5dd
Josef Friedrich <josef@friedrich.rocks>
2016-11-05 11:57:25 +0100

    Include scripts from external github repositories
    This repositories are now obsolete and will soon be deleted:
    https://github.com/Josef-Friedrich/zfs-diff-walkthrough
    https://github.com/Josef-Friedrich/wordpress-update-url

---
+++ wordpress-url-update.sh
@@ -0,0 +1,192 @@
+#! /bin/sh
+
+# MIT License
+#
+# Copyright (c) 2016 Josef Friedrich <josef@friedrich.rocks>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+##
+# wordpress-update-url
+#
+# Author: Josef Friedrich <jf@josef-friedrich.de>
+# Version: 1.0
+# Date: 2014.12.31
+##
+
+MYSQL_USER=""
+MYSQL_PASSWORD=""
+MYSQL_DATABASE=""
+NEW_URL=""
+
+_execute_mysql() {
+	mysql \
+		--silent \
+		--raw \
+		--user=$MYSQL_USER \
+		--password=$MYSQL_PASSWORD $MYSQL_DATABASE \
+		-e "$1" 2> /dev/null
+}
+
+_get_old_url() {
+	_execute_mysql "
+		SELECT option_value FROM options WHERE option_name = 'siteurl';
+	"
+}
+
+_update_wp_options() {
+	_execute_mysql "
+		UPDATE options
+			SET option_value = replace(option_value, '$OLD_URL', '$NEW_URL')
+			WHERE option_name = 'home'
+				OR option_name = 'siteurl';
+	"
+}
+
+_update_wp_posts() {
+	_execute_mysql "
+		UPDATE posts
+		SET guid = replace(guid, '$OLD_URL','$NEW_URL');
+"
+}
+
+_update_post_content() {
+	_execute_mysql "
+		UPDATE posts
+		SET post_content = replace(post_content, '$OLD_URL', '$NEW_URL');
+	"
+}
+
+_usage() {
+	echo "Usage: $0
+
+Options:
+	-u MySQL user
+	-p MySQL password
+	-d MySQL database
+	-o Old URL
+	-n New URL
+	-h Show usage
+
+	wordpress-update-url
+	====================
+
+	__wordpress-update-url__ is a small shell script to update the url of
+	wordpress sites.
+
+	This script uses the mysql shell command. To use this script you must have
+	access to the mysql server providing the data for your wordpress site
+	over the shell command.
+
+	# Where is the url stored in the mysql database?
+
+	* In the table ``wp_options`` in the column ``option_value``.
+	* In the table ``wp_posts`` in the columns ``guid`` and ``post_content``.
+
+	# Command line usage:
+
+	```
+	./wordpress-update-url -u <user> -p <password> -d <database> -n <new-url>
+	```
+
+	## Example:
+
+	```
+	./wordpress-update-url -u root -p 5dtaJ -d wp_db -n http://new-url.com
+	```
+
+	If you use the shell script frequently on the same site, it is recommended
+	to edit the script file and put there your mysql connection and url
+	informations:
+
+	```
+	MYSQL_USER=""
+	MYSQL_PASSWORD=""
+	MYSQL_DATABASE=""
+	NEW_URL=""
+	```
+
+	## Example:
+
+	```
+	MYSQL_USER="root"
+	MYSQL_PASSWORD="5dtaJ"
+	MYSQL_DATABASE="wp_db"
+	NEW_URL="http://new-url.com"
+	```
+
+	Then you can update your wordpress site executing this short command:
+
+	```
+	./wordpress-update-url
+	```
+"
+}
+
+while getopts ":u:p:d:o:n:h" OPT; do
+	case $OPT in
+
+		u) MYSQL_USER="$OPTARG";;
+		p) MYSQL_PASSWORD="$OPTARG";;
+		d) MYSQL_DATABASE="$OPTARG";;
+		o) OLD_URL="$OPTARG";;
+		n) NEW_URL="$OPTARG";;
+		h) _usage;;
+
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+
+	esac
+done
+
+if [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ]; then
+	echo "No or incomplete informations about the MySQL connection!
+Use -u (user) -p (password) and -d (database) options."
+	exit 1
+fi
+
+if [ -z "$OLD_URL" ]; then
+	OLD_URL=$(_get_old_url)
+fi
+
+if [ -z "$NEW_URL" ]; then
+	echo "No new URL! Use -n option."
+	exit 1
+fi
+
+# For debug purposes only:
+#echo "MySQL user: $MYSQL_USER"
+#echo "MySQL password: $MYSQL_PASSWORD"
+#echo "MySQL database: $MYSQL_DATABASE"
+#echo "Old URL: $OLD_URL"
+#echo "New URL: $NEW_URL"
+
+_update_wp_options
+
+_update_wp_posts
+
+_update_post_content
---
+++ zfs-diff-walkthrough.sh
@@ -0,0 +1,120 @@
+#! /bin/bash
+
+# MIT License
+#
+# Copyright (c) 2016 Josef Friedrich <josef@friedrich.rocks>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+_get_snapshots() {
+	if [ -n "$DIRECTORY"]; then
+		DIRECTORY=$(pwd)
+	fi
+	readarray -t SNAPSHOTS <<< "$(zfs list -t snapshot -o name -r -H "$DIRECTORY")"
+}
+
+_get_date() {
+	zfs get -H -o value creation "$1"
+}
+
+_list_snapshots() {
+	((COUNT=${#SNAPSHOTS[@]}, MAX=COUNT - 1))
+
+	for ((i = 0; i <= MAX; i++)); do
+		echo -e "$i:\t${SNAPSHOTS[i]}"
+	done
+}
+
+_usage() {
+echo "Usage: zfs-diff-walkthrough [-p] <nr> [<nr>]
+
+Options:
+  -d   Dataset or directory.
+  -h   Show this help message.
+  -p   Compare with previous snapshot instead of later snapshot.
+
+"
+}
+
+while getopts ":d:hp" OPT; do
+	case $OPT in
+
+	d)
+		DIRECTORY="$OPTARG"
+		;;
+	h)
+		_usage
+		exit 0
+		;;
+
+	p)
+		DIFF_PREVIOUS=1
+		;;
+
+	\?)
+		echo "Invalid option: -$OPTARG" >&2
+		exit 1
+		;;
+
+	:)
+		echo "Option -$OPTARG requires an argument." >&2
+		exit 1
+		;;
+
+	esac
+done
+
+shift $((OPTIND-1))
+
+FIRST=$1
+SECOND=$2
+
+_get_snapshots
+
+if [ -z "$FIRST" ]; then
+	_usage
+	_list_snapshots
+	exit 1
+fi
+
+if [ -z "$SECOND" ]; then
+	if [ -n "$DIFF_PREVIOUS" ]; then
+		SECOND=$FIRST
+		FIRST=$((FIRST - 1))
+	else
+		SECOND=$((FIRST + 1))
+	fi
+fi
+
+SNAP_FIRST="${SNAPSHOTS[$FIRST]}"
+SNAP_SECOND="${SNAPSHOTS[$SECOND]}"
+
+echo "Differences between:
+
+  $SNAP_FIRST   [$(_get_date $SNAP_FIRST)]
+
+    <- and ->
+
+  $SNAP_SECOND   [$(_get_date $SNAP_SECOND)]
+
+###########################################################
+"
+
+zfs diff "$SNAP_FIRST" "$SNAP_SECOND"
