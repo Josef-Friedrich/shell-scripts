53cb4509784b4a43e63d0885d59d20f3383c45b3
Josef Friedrich <josef@friedrich.rocks>
2016-11-05 12:12:26 +0100

    Make documentation better

--- cue-split.sh
+++ cue-split.sh
@@ -2,13 +2,45 @@
 
 # https://bbs.archlinux.org/viewtopic.php?id=75774
 
-# frontend for:	cuetools, shntool, mp3splt
-# optional dependencies:	flac, mac, wavpack, ttaenc
-# v1.3 sen
+# MIT License
+#
+# Copyright (c) 2016 Josef Friedrich <josef@friedrich.rocks>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 CUETAG=cuetag
-
+FLAC='flac flac -V --best -o %f -'
+FORMAT='%n %p - %t'
 SDIR=`pwd`
+
+_usage() {
+	echo "Usage: $(basename "$0") [Path]
+
+frontend for:	cuetools, shntool, mp3splt
+optional dependencies:	flac, mac, wavpack, ttaenc
+
+The default path is the current directory.
+
+The folder must contain only one *.cue file and one audio file.
+"
+}
 
 if [ "$1" = "" ]
 	then
@@ -16,12 +48,7 @@ if [ "$1" = "" ]
 else
 		case $1 in
 				-h | --help )
-						echo "Usage: $(basename "$0") [Path]
-
-The default path is the current directory.
-
-The folder must contain only one *.cue file and one audio file.
-"
+						_usage
 						exit
 						;;
 				* )
@@ -38,7 +65,7 @@ TYPE=`ls -t1`
 case $TYPE in
 	*.ape*)
 		mkdir split
-		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.ape -t "%n %p - %t"
+		shnsplit -d split -f *.cue -o "$FLAC" *.ape -t "$FORMAT"
 		rm -f split/00*pregap*
 		"$CUETAG" *.cue split/*.flac
 		exit
@@ -46,7 +73,7 @@ case $TYPE in
 
 	*.flac*)
 		mkdir split
-		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.flac -t "%n %p - %t"
+		shnsplit -d split -f *.cue -o "$FLAC" *.flac -t "$FORMAT"
 		rm -f split/00*pregap*
 		"$CUETAG" *.cue split/*.flac
 		exit
@@ -66,7 +93,7 @@ case $TYPE in
 
 	*.tta*)
 		mkdir split
-		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.tta -t "%n %p - %t"
+		shnsplit -d split -f *.cue -o "$FLAC" *.tta -t "$FORMAT"
 		rm -f split/00*pregap*
 		"$CUETAG" *.cue split/*.flac
 		exit
@@ -74,7 +101,7 @@ case $TYPE in
 
 	*.wv*)
 		mkdir split
-		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wv -t "%n %p - %t"
+		shnsplit -d split -f *.cue -o "$FLAC" *.wv -t "$FORMAT"
 		rm -f split/00*pregap*
 		"$CUETAG" *.cue split/*.flac
 		exit
@@ -82,7 +109,7 @@ case $TYPE in
 
 	*.wav*)
 		mkdir split
-		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wav -t "%n %p - %t"
+		shnsplit -d split -f *.cue -o "$FLAC" *.wav -t "$FORMAT"
 		rm -f split/00*pregap*
 		"$CUETAG" *.cue split/*.flac
 		exit
@@ -93,6 +120,3 @@ case $TYPE in
 		echo "			 --> APE, FLAC, MP3, OGG, TTA, WV, WAV"
 
 esac
-
-exit
-‚
