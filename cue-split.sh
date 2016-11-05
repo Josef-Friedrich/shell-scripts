04f2ad10cd9b4f24a80d40aa4121d3a845b3a868
Josef Friedrich <josef@friedrich.rocks>
2016-11-05 12:04:47 +0100

    Refactor

--- cue-split.sh
+++ cue-split.sh
@@ -2,8 +2,8 @@
 
 # https://bbs.archlinux.org/viewtopic.php?id=75774
 
-# frontend for:            cuetools, shntool, mp3splt
-# optional dependencies:    flac, mac, wavpack, ttaenc
+# frontend for:	cuetools, shntool, mp3splt
+# optional dependencies:	flac, mac, wavpack, ttaenc
 # v1.3 sen
 
 CUETAG=cuetag
@@ -11,88 +11,88 @@ CUETAG=cuetag
 SDIR=`pwd`
 
 if [ "$1" = "" ]
-  then
-    DIR=$SDIR
+	then
+		DIR=$SDIR
 else
-    case $1 in
-        -h | --help )
-            echo "Usage: $(basename "$0") [Path]
-	    
+		case $1 in
+				-h | --help )
+						echo "Usage: $(basename "$0") [Path]
+
 The default path is the current directory.
 
 The folder must contain only one *.cue file and one audio file.
 "
-            exit
-            ;;
-        * )
-        DIR=$1
-    esac
+						exit
+						;;
+				* )
+				DIR=$1
+		esac
 fi
 
-echo "
-
-Directory: $DIR
-________________________________________
+echo "Directory: $DIR
 "
 
 cd "$DIR"
 TYPE=`ls -t1`
 
 case $TYPE in
-    *.ape*)
-        mkdir split
-        shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.ape -t "%n %p - %t"
-        rm -f split/00*pregap*
-        "$CUETAG" *.cue split/*.flac
-        exit
-        ;;
+	*.ape*)
+		mkdir split
+		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.ape -t "%n %p - %t"
+		rm -f split/00*pregap*
+		"$CUETAG" *.cue split/*.flac
+		exit
+		;;
 
-    *.flac*)
-        mkdir split
-        shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.flac -t "%n %p - %t"
-        rm -f split/00*pregap*
-        "$CUETAG" *.cue split/*.flac
-        exit
-        ;;
+	*.flac*)
+		mkdir split
+		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.flac -t "%n %p - %t"
+		rm -f split/00*pregap*
+		"$CUETAG" *.cue split/*.flac
+		exit
+		;;
 
-    *.mp3*)
-        mp3splt -no "@n @p - @t (split)" -c *.cue *.mp3
-        "$CUETAG" *.cue *split\).mp3
-        exit
-        ;;
+	*.mp3*)
+		mp3splt -no "@n @p - @t (split)" -c *.cue *.mp3
+		"$CUETAG" *.cue *split\).mp3
+		exit
+		;;
 
-    *.ogg*)
-        mp3splt -no "@n @p - @t (split)" -c *.cue *.ogg
-        "$CUETAG" *.cue *split\).ogg
-        exit
-        ;;
+	*.ogg*)
+		mp3splt -no "@n @p - @t (split)" -c *.cue *.ogg
+		"$CUETAG" *.cue *split\).ogg
+		exit
+		;;
 
-    *.tta*)
-        mkdir split
-        shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.tta -t "%n %p - %t"
-        rm -f split/00*pregap*
-        "$CUETAG" *.cue split/*.flac
-        exit
-        ;;
+	*.tta*)
+		mkdir split
+		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.tta -t "%n %p - %t"
+		rm -f split/00*pregap*
+		"$CUETAG" *.cue split/*.flac
+		exit
+		;;
 
-    *.wv*)
-        mkdir split
-        shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wv -t "%n %p - %t"
-        rm -f split/00*pregap*
-        "$CUETAG" *.cue split/*.flac
-        exit
-        ;;
+	*.wv*)
+		mkdir split
+		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wv -t "%n %p - %t"
+		rm -f split/00*pregap*
+		"$CUETAG" *.cue split/*.flac
+		exit
+		;;
 
-    *.wav*)
-        mkdir split
-        shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wav -t "%n %p - %t"
-        rm -f split/00*pregap*
-        "$CUETAG" *.cue split/*.flac
-        exit
-        ;;
+	*.wav*)
+		mkdir split
+		shnsplit -d split -f *.cue -o "flac flac -V --best -o %f -" *.wav -t "%n %p - %t"
+		rm -f split/00*pregap*
+		"$CUETAG" *.cue split/*.flac
+		exit
+		;;
 
-    * )
-    echo "Error: Found no files to split!"
-    echo "       --> APE, FLAC, MP3, OGG, TTA, WV, WAV"
+	* )
+		echo "Error: Found no files to split!"
+		echo "			 --> APE, FLAC, MP3, OGG, TTA, WV, WAV"
+
 esac
+
 exit
+‚
